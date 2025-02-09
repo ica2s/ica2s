6d6e5a517eff0560c639ab7577263291f0145c15
techynidhi <nidhi129kumari@gmail.com>
2025-02-09 16:16:13 +0530

    added the photos and also modified the contact page

--- src/js/main.js
+++ src/js/main.js
@@ -28,3 +28,24 @@ menuLinks.forEach((link) => {
 //   });
 // });
 
+// document.querySelector("#contactForm").addEventListener("submit", async (e) => {
+//   e.preventDefault();
+
+//   const name = document.querySelector("#name").value;
+//   const email = document.querySelector("#email").value;
+//   const message = document.querySelector("#message").value;
+
+//   const response = await fetch("http://localhost:5000/api/contact", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ name, email, message }),
+//   });
+
+//   const result = await response.json();
+
+//   if (response.ok) {
+//       alert("Message sent successfully!");
+//   } else {
+//       alert("Error: " + result.error);
+//   }
+// });
