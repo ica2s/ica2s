90a233ea57e552c3080c7222bd227d67b6b83c12
Techy Nidhi <nidhi129kumari@gmail.com>
2025-05-15 23:27:28 +0530

    modified the vite.config.js file according to changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  build:{
-    rollupOptions:{
-      input:{
-        main: 'index.html',
-        committee:'committee/index.html',
-        call: 'call/index.html',
-        registration: 'registration/index.html',
-        guidelines: 'guidelines/index.html',
-        updates: 'updates/index.html'
-      }
-    }
-  }
-})
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        committee: "committee/index.html",
+        call: "call/index.html",
+        registration: "registration/index.html",
+        guidelines: "guidelines/index.html",
+        updates: "updates/index.html",
+        specialSession: "special-sessions/index.html",
+      },
+    },
+  },
+});
