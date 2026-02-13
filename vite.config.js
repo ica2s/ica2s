db77b2232ce7bbcfa270c07014e66ef09eb1a3ee
Techy Nidhi <nidhi129kumari@gmail.com>
2026-02-14 02:59:22 +0530

    path fixed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
+  base: "/", // IMPORTANT
   plugins: [tailwindcss()],
   build: {
     rollupOptions: {
@@ -8,12 +10,13 @@ export default defineConfig({
         main: "index.html",
         y2025: "2025/index.html",
         y2026: "2026/index.html",
-        // committee: "committee/index.html",
-        // call: "call/index.html",
-        // registration: "registration/index.html",
-        // guidelines: "guidelines/index.html",
-        // updates: "updates/index.html",
-        // specialSession: "special-sessions/index.html",
+
+        committee2025: "2025/committee/index.html",
+        call2025: "2025/call/index.html",
+        registration2025: "2025/registration/index.html",
+        guidelines2025: "2025/guidelines/index.html",
+        updates2025: "2025/updates/index.html",
+        special2025: "2025/special-sessions/index.html",
       },
     },
   },
